dd748a6dca8ec3579ce61f689be47da46eb6c74a
SomeRandomDev <41921648+ChangDaddy@users.noreply.github.com>
2021-11-06 21:01:27 -0400

    Pushed

---
+++ src/main/java/strafed/us/Strafed.java
@@ -0,0 +1,18 @@
+package strafed.us;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Strafed extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+
+        super.onDisable();
+    }
+}
